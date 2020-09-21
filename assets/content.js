744f2552f3d43c4448e434478911cae96d03920a
Barbara Neves <barbara.neves.oliveira@gmail.com>
2020-09-21 20:59:57 -0300

    Extension Chrome Angry-Birds Rectified

--- assets/content.js
+++ assets/content.js
@@ -6,21 +6,24 @@ chrome.runtime.onMessage.addListener(replace);
 
 
 function replace() {
-  contadorNum = 0;
+  contNum = 0;
   let imgs = document.getElementsByTagName('img'); 
   for(imgElt of imgs) { 
      
-     
+      let h = imgElt.height;
+      let w = imgElt.width;
       let file = "../images/angry-birds.png"; 
       let url = chrome.extension.getURL(file); 
       imgElt.src = url; 
+      imgElt.height = h;
+      imgElt.width = w;
       console.log(url);   
-      contadorNum = contadorNum +1;
-      console.log(contadorNum);
+      contNum = contNum +1;
+      console.log(contNum);
     } 
 
     const child = document.createElement('h1');
-    child.textContent = " Number of images : " + contadorNum;
+    child.textContent = " Number of images : " + contNum;
     child.style.color = 'blue';
     child.style.backgroundSize= '10em';
     child.style.background = 'black';
